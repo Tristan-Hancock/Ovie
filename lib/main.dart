8f3425f82864a5400c4b4306c0c97c322340bcbe
Tristan <tristanhancock@gmail.com>
2024-06-21 03:24:04 +0530

    Fixed Bottom Navigation

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'widgets/background_gradient.dart';
 import 'widgets/bottom_navigation.dart';
 
 void main() {
@@ -19,12 +18,36 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  int _selectedIndex = 0;
+
+  static const List<Widget> _widgetOptions = <Widget>[
+    Text('Home Page'),
+    Text('Calendar Page'),
+    Text('Group Page'),
+    Text('Profile Page'),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: BackgroundGradient(
-        child: BottomNavigation(),
+      body: Center(
+        child: _widgetOptions.elementAt(_selectedIndex),
+      ),
+      bottomNavigationBar: BottomNavigation(
+        selectedIndex: _selectedIndex,
+        onItemTapped: _onItemTapped,
       ),
     );
   }
--- lib/widgets/bottom_navigation.dart
+++ lib/widgets/bottom_navigation.dart
@@ -2,57 +2,46 @@ import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class BottomNavigation extends StatefulWidget {
+  final int selectedIndex;
+  final Function(int) onItemTapped;
+
+  BottomNavigation({
+    required this.selectedIndex,
+    required this.onItemTapped,
+  });
+
   @override
   _BottomNavigationState createState() => _BottomNavigationState();
 }
 
 class _BottomNavigationState extends State<BottomNavigation> {
-  int _selectedIndex = 0;
-
-  static const List<Widget> _widgetOptions = <Widget>[
-    Text('Home Page'),
-    Text('Calendar Page'),
-    Text('Group Page'),
-    Text('Profile Page'),
-  ];
-
-  void _onItemTapped(int index) {
-    setState(() {
-      _selectedIndex = index;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: _widgetOptions.elementAt(_selectedIndex),
-      ),
-      bottomNavigationBar: BottomNavigationBar(
-        items: const <BottomNavigationBarItem>[
-          BottomNavigationBarItem(
-            icon: FaIcon(FontAwesomeIcons.home),
-            label: 'Home',
-          ),
-          BottomNavigationBarItem(
-            icon: FaIcon(FontAwesomeIcons.calendar),
-            label: 'Calendar',
-          ),
-          BottomNavigationBarItem(
-            icon: FaIcon(FontAwesomeIcons.users),
-            label: 'Community',
-          ),
-          BottomNavigationBarItem(
-            icon: FaIcon(FontAwesomeIcons.user),
-            label: 'Account',
-          ),
-        ],
-        currentIndex: _selectedIndex,
-        selectedItemColor: Colors.black,
-        unselectedItemColor: Colors.grey,
-        onTap: _onItemTapped,
-        backgroundColor: Color(0xFFFCD9DA),
-      ),
+    return BottomNavigationBar(
+      type: BottomNavigationBarType.fixed, // Ensure the type is fixed
+      items: const <BottomNavigationBarItem>[
+        BottomNavigationBarItem(
+          icon: FaIcon(FontAwesomeIcons.home),
+          label: 'Home',
+        ),
+        BottomNavigationBarItem(
+          icon: FaIcon(FontAwesomeIcons.calendar),
+          label: 'Calendar',
+        ),
+        BottomNavigationBarItem(
+          icon: FaIcon(FontAwesomeIcons.users),
+          label: 'Community',
+        ),
+        BottomNavigationBarItem(
+          icon: FaIcon(FontAwesomeIcons.user),
+          label: 'Account',
+        ),
+      ],
+      currentIndex: widget.selectedIndex,
+      selectedItemColor: Colors.black,
+      unselectedItemColor: Colors.grey,
+      onTap: widget.onItemTapped,
+      backgroundColor: Color(0xFFF9EAEA), // Set the background color
     );
   }
 }
